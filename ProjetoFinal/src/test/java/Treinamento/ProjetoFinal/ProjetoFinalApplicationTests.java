666c166d5e050877c6b097a10971ae1f291c4067
Jacqueline Garutti <67115372+JacqueGarutti@users.noreply.github.com>
2020-06-30 18:46:23 -0200

    SpringBoot do projeto final -30.06

---
+++ ProjetoFinal/src/test/java/Treinamento/ProjetoFinal/ProjetoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package Treinamento.ProjetoFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
