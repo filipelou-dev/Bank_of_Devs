814ca30dcca1e8c1164dd5c13eed23c3928c9d2b
Jacqueline Garutti <67115372+JacqueGarutti@users.noreply.github.com>
2020-06-30 19:22:00 -0200

    Inserção usuarios
    Script com valores  a serem inseridos na tabela usuarios

---
+++ scrit inserir valores.sql
@@ -0,0 +1,10 @@
+ 
+    insert into tbl_usuario values (null,'filipelou.f@gmail.com','Filipe','fililou','q1w2e3r4');
+    insert into tbl_usuario values (null,'jacquegarutti@gmail.com','Jacque','jaclope','12345');
+    insert into tbl_usuario values (null,'Daniel@gmail.com','Daniel','danidmy','doti123');
+	insert into tbl_usuario values (null,'fellypeRossatti@gmail.com','Fellype','rossfel','fellype123');
+    insert into tbl_usuario values (null,'leandro@gmail.com','Leandro','leabass','starwars');
+
+	select* from tbl_usuario;
+      
+    
